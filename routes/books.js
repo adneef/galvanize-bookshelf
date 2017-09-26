5656fc4dc9666c07a5f4d241bd382cbcf59b6b9b
Adam Neef <adneef@gmail.com>
2017-09-26 10:03:18 -0600

    working on get

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,19 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-
-// eslint-disable-next-line new-cap
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
 
 // YOUR CODE HERE
+router.get('/', function(req, res, next) {
+  knex('books')
+    .select('id', 'title', 'author', 'genre', 'description', 'cover_url', 'created_at', 'updated_at')
+    .orderBy('id')
+    .then((items) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(books))
+  })
+  .catch((err) => next(err)
+})
 
-module.exports = router;
+module.exports = router

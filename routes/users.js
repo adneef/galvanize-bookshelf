bbcea3a26eeb56614a2abd2409295a71b7905831
Adam Neef <adneef@gmail.com>
2017-09-28 09:17:23 -0600

    working on part 3

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const express = require('express');
-
-// eslint-disable-next-line new-cap
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+const bcrypt = require('bcrypt')
 
 // YOUR CODE HERE
+router.post('/users', function(req, res, next) {
+  let {first_name, last_name, email, password} = req.body
+
+})
 
 module.exports = router;

704f3a3f41db2c952e9e06887be470dc83d6c024
Adam Neef <adneef@gmail.com>
2017-09-28 09:50:25 -0600

    part 3 complete

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,28 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
 const bcrypt = require('bcrypt')
+const humps = require('humps')
 
 // YOUR CODE HERE
 router.post('/users', function(req, res, next) {
-  let {first_name, last_name, email, password} = req.body
+  let first_name = humps.decamelizeKeys(req.body.firstName)
+  let last_name = humps.decamelizeKeys(req.body.lastName)
+  let email = req.body.email
+  let password = req.body.password
+
+bcrypt.hash(password, 6, function(err, hash) {
+  knex('users')
+  .returning(['id', 'first_name', 'last_name', 'email'])
+    .insert({first_name: first_name,
+      last_name: last_name,
+      email: email,
+      hashed_password: hash})
+    .then((user) => {
+
+  res.send(humps.camelizeKeys(user[0]))
+  })
+  .catch((err) => next(err))
+})
 
 })
 

00d1df331bdd089b46afddf9979833cd0fe37bc7
Adam Neef <adneef@gmail.com>
2017-09-29 10:26:46 -0600

    progress on part4. slowly.

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,37 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+const bcrypt = require('bcrypt')
+const humps = require('humps')
+const jwt = require('jsonwebtoken')
+const secret = process.env.JWT_KEY
 
-// eslint-disable-next-line new-cap
-const router = express.Router();
+//POST to create token
+router.post('/token', function(req, res, next) {
+  knex('users')
+    .where('email' = req.body.email)
+    .first()
+    .then((data) => {
+      let match = bcrypt.compareSync(req.body.password, data.hashed_password)
+       if (!match) {
+         res.cookies.token = jwt.sign({userId: data.id}, secret)
+       }
+    })
 
-// YOUR CODE HERE
+})
 
-module.exports = router;
+
+
+// GET request and verify token
+router.get('/token', function(req, res, next) {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, function(err, payload) {
+    if (err) {
+      return res.send(false)
+    }
+    res.send(true)
+  })
+})
+
+module.exports = router
